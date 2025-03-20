d809d6bc90ddeb5b4d390bbc909f7c1a27443309
Isaaq Simon <IsaaqS>
2025-03-20 14:53:28 +0000

    Math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+    document.getElementById('multiply-math1').innerHTML = 'The area of the rectangle is ' + (24 * 65)
+    document.getElementById('multiply-math2').innerHTML = 'The perimeter of the rectangle is ' + (2 * 24 + 2 * 65)
+}
+
