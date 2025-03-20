f4d3927b0e0bef4c97f1fc343619e42bf8ba2466
Isaaq Simon <IsaaqS>
2025-03-20 15:04:05 +0000

    Math

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 function doMathClicked () {
   // this function does basic math
-    document.getElementById('multiply-math1').innerHTML = 'The area of the rectangle is ' + (24 * 65)
-    document.getElementById('multiply-math2').innerHTML = 'The perimeter of the rectangle is ' + (2 * 24 + 2 * 65)
+    document.getElementById('multiply-math1').innerHTML = 'The area of the rectangle is ' + (24 * 65) + 'cm²'
+    document.getElementById('multiply-math2').innerHTML = 'The perimeter of the rectangle is ' + (2 * 24 + 2 * 65) + 'cm'
 }
 
